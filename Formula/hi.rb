5ab271ea2c47c044176ef1d2175069d23330875a
Kirilkovale1424143 <155623484+Kirilkovale1424143@users.noreply.github.com>
2026-06-10 20:28:45 +0300

    Change sha256 to :no_check for hi.rb

--- Formula/hi.rb
+++ Formula/hi.rb
@@ -3,7 +3,7 @@ class Hi < Formula
   homepage "https://github.com/Kirilkovale1424143/hi"
   url "https://github.com/Kirilkovale1424143/hi/releases/download/Spam/hi"
   version "1.0.0"
-  sha256 "9a3f37f893d87a7b25727751475deeea7b8b7a0236036ac8b191dde5ff2e95e8"
+  sha256 :no_check
   license :cannot_represent
 
   depends_on :xcode => ["12.0", :build]
