5328cc2c0f51b8db6cbd38ca8428cada09332212
Kirilkovale1424143 <155623484+Kirilkovale1424143@users.noreply.github.com>
2026-06-10 20:30:04 +0300

    Update sha256 checksum for hi.rb

--- Formula/hi.rb
+++ Formula/hi.rb
@@ -3,7 +3,7 @@ class Hi < Formula
   homepage "https://github.com/Kirilkovale1424143/hi"
   url "https://github.com/Kirilkovale1424143/hi/releases/download/Spam/hi"
   version "1.0.0"
-  sha256 :no_check
+  sha256 "cbb2050b7628cdb64c57c0bf8611c5e0377c8b2701c165b4b441bd1161c3a7c8"
   license :cannot_represent
 
   depends_on :xcode => ["12.0", :build]
